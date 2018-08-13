e577dcdca4dafbb6fc462552b8555ab5f38d450b
Vinicius Azevedo <viniazvd@gmail.com>
2018-08-12 23:55:38 -0300

    fix - to standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,7 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error': 'off',
-    "vue/no-side-effects-in-computed-properties": "off"
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
