7e303b554000d53f6bba4ca69fd617bad214aa9c
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-08-13 17:24:46 -0300

    by @VitorLuizC

--- src/helpers/order.js
+++ src/helpers/order.js
@@ -1,22 +1,11 @@
 import isNumber from './isNumber'
 
-// const toggleOrder = (row, icon) => (a, b) => {
-//   const typeOfNumber = isNumber(a[row], b[row])
-
-//   if (icon === '▼' && typeOfNumber) return a[row] - b[row]
-//   if (icon === '▲' && typeOfNumber) return b[row] - a[row]
-//   if (icon === '▲' && !typeOfNumber) return a[row] < b[row]
-//   if (icon === '▼' && !typeOfNumber) return a[row] > b[row]
-// }
-
-const condition = (isA, a, b) => isA ? a : b
-
 const toggleOrder = (row, icon) => (a, b) => {
-  return condition(
-    isNumber(a[row], b[row]),
-    condition(icon === '▲', a[row] - b[row], b[row] - a[row]),
-    condition(icon === '▲', a[row] < b[row], a[row] > b[row])
-  )
+  if (isNumber(a[row], b[row])) {
+    return icon === '▲' ? b[row] - a[row] : a[row] - b[row]
+  }
+
+  return icon === '▲' ? a[row] < b[row] : a[row] > b[row]
 }
 
 export default toggleOrder
