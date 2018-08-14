da50154868a06e9a8c5fffd5ec583814a6e5cedb
Vinicius Azevedo <viniazvd@gmail.com>
2018-08-13 23:15:22 -0300

    upgrade data

--- src/example/data-table.js
+++ src/example/data-table.js
@@ -1,10 +1,3 @@
-export const trColStyle = { 'background-color': '#E7E9F0' }
-export const thColStyle = { 'color': '#5E6784', 'min-width': '100px' }
-export const trRowStyle = { 'border': '1px solid #E7E9F0' }
-export const tdRowStyle = { 'text-align': 'center' }
-export const rowStyle = { 'color': '#5E6784' }
-export const totalStyle = { 'color': '#5E6784', 'background-color': '#E7E9F0' }
-
 export const cols = [
   { label: 'Nome', row: 'name' },
   { label: 'E-mail', row: 'email' },
@@ -16,7 +9,25 @@ export const rows = [
   { name: 'coe1', email: 'coe1.mane@gmail.com', age: 6 },
   { name: 'coe2', email: 'coe2.mane@gmail.com', age: 3, malice: 3 },
   { name: 'coe3', email: 'coe3.mane@gmail.com', age: 2 },
-  { name: 'coe4', email: 'coe4.mane@gmail.com', age: 1, malice: 10 }
+  { name: 'coe4', email: 'coe4.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe5', email: 'coe5.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe6', email: 'coe6.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe7', email: 'coe7.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe8', email: 'coe8.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe9', email: 'coe9.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe10', email: 'coe10.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe11', email: 'coe11.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe12', email: 'coe12.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe13', email: 'coe13.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe14', email: 'coe14.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe15', email: 'coe15.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe16', email: 'coe16.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe17', email: 'coe17.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe18', email: 'coe18.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe19', email: 'coe19.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe20', email: 'coe20.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe21', email: 'coe21.mane@gmail.com', age: 1, malice: 10 },
+  { name: 'coe22', email: 'coe22.mane@gmail.com', age: 1, malice: 10 }
 ]
 
-export const total = { enable: true, label: 'Total', colPosition: 'email' }
+export const total = { label: 'Total', colPosition: 'email' }
