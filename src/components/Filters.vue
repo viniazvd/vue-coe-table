a984d3128ce4d691113e09c639efb152ab047c0d
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-08-17 18:12:47 -0300

    init

---
+++ src/components/Filters.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="filters">
+    <c-input 
+      class="filter"
+      v-bind="$attrs"
+      :value="search"
+      @input="value => $emit('search', value)"
+    />
+  </div>
+</template>
+
+<script>
+import CInput from './Base/CInput'
+
+export default {
+  name: 'filters',
+
+  components: { CInput },
+
+  props: {
+    search: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.filters {
+  display: flex;
+  margin-bottom: 20px;
+
+  & > .filter {}
+}
+</style>
